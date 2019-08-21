9667865d1e5fb083ae885d7fe97587ba6c548ec3
Jader Froes <jfdsrosa@inf.ufpel.edu.br>
2019-08-21 14:30:17 -0300

    tarefa basica 1

---
+++ tarefa1.py
@@ -0,0 +1,18 @@
+# fazer expressões regulares que captam email e telefone do site da ufpel
+import re
+
+email = "t2oretorebaixado5cm@bol.com"
+telefone = "12345-67890"
+
+flag_email = re.search("^[a-z][a-z0-9]+@[a-z]+.com$", email)
+flag_telefone = re.search("^[1-9][0-9]{4}-[0-9]{4}$", telefone)
+
+if(flag_email):
+    print("email válido")
+else:
+    print("email não aprovado")
+
+if(flag_telefone):
+    print("telefone válido")
+else:
+    print("telefone não aprovado")
